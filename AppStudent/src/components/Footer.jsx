6e235299199d1c2d8d0de2a0bf5476eeaebcf8e7
Axel Paredes <paredes.axel456@gmail.com>
2024-05-05 16:01:52 +0800

    apdit

--- AppStudent/src/components/Footer.jsx
+++ AppStudent/src/components/Footer.jsx
@@ -4,8 +4,7 @@ import '../index.css'
 function Footer() {
     return (
       <div className='w-full bg-slate-700 h-10 flex items-center justify-center'>
-        <label class="text-white text-center w-full"
-                        >&copy; Copyright 2024 Axel Paredes</label>
+        <label class="text-white text-center w-full">&copy; Copyright 2024 Axel Paredes</label>
       </div>
     );
   }
